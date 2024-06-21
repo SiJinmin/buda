350d0d2959192ec86055cc0d573582bf4fe65556
SiJinmin <jinmin.si@outlook.com>
2024-06-21 16:35:16 +0800

    update_vue

--- vue/src/jobs.js
+++ vue/src/jobs.js
@@ -1,71 +1,10 @@
-let jobs=[
-  
-  { 
-    company:"艺术速递",
-    location:'金银湖科技园2栋602',
-    summary:'',
-    env:['艺术速递大门.jpg', '艺术速递内景.jpg', '艺术速递logo.jpg'],  
-    contacts:['本群@周林平'],  
-    closed: true,
-  },
-  { 
-    company:"玲明信息技术服务",
-    location:'金银湖科技园16栋',
-    summary:'提供信息技术服务:',
-    ServiceItems: ['电脑系统安装维护','Excel/Word等文件资料自动化处理','文件存储管理系统（历史记录、多人协作）','网站开发','管理软件开发','工作流程自动化软件开发','微信小程序开发','手机APP开发'],
-    env:['qiuzen20240613.jpg'],  
-    contacts:['微信：TobeBuda'],  
-    closed: true,
-  },
-  { 
-    company:"品牌女装",
-    location:'金银湖科技园7栋4单元3楼',
-    summary:'',
-    env:['品牌女装.jpg'],  
-    closed: true,
-  },
+let jobs=[  
   { 
     company:"普诺克精密工具",
     location:'金银湖科技园9栋1楼',
     summary:'招聘能上12小时夜班的年轻人',
     env:['普诺克前台.jpg'],
     contacts:['微信群@大宝'],
-  },
-  { 
-    company:"武汉科印",
-    location:'金银湖科技园12栋',
-    env:['武汉科印办公室.jpg', '武汉科印大门.jpg', '武汉科印大厅.jpg', '武汉科印礼品架.jpg', '飞控自动化展厅.jpg', '飞控自动化走廊.jpg'],    
-    closed: true,
-  },
-  { 
-    company:"科电中威电气",
-    location:'金银湖科技园12栋',
-    env:['科电中威电气大门.jpg'],    
-    closed: true,
-  },
-  { 
-    company:"凯迪正大电气",
-    location:'金银湖科技园12栋',
-    env:['凯迪正大电气电梯.jpg', '凯迪正大电气前台.jpg', '凯迪正大电气植物.jpg', '飞控自动化会议室.jpg', '飞控自动化展厅.jpg', '飞控自动化走廊.jpg'],    
-    closed: true,
-  },
-  { 
-    company:"定制盒饭团餐",
-    location:'',
-    env:['定制盒饭团餐.jpg'],    
-    closed: true,
-  },
-  { 
-    company:"飞控自动化工程",
-    location:'金银湖科技园12栋',
-    env:['飞控自动化大厅.jpg', '飞控自动化电梯.jpg', '飞控自动化花架.jpg', '飞控自动化会议室.jpg', '飞控自动化展厅.jpg', '飞控自动化走廊.jpg'],    
-    closed: true,
-  },
-  { 
-    company:"达译自动化工程",
-    location:'金银湖科技园12栋',
-    env:['达译自动化.jpg'],    
-    closed: true,
   },
   { 
     company:"武汉天怡食品",
@@ -80,21 +19,6 @@ let jobs=[
     summary:'招聘车间普工，4k-7k，五险一金，包食宿',
     detail:['优点生物普工.jpg'],
     env:['优点生物大门.jpg'],
-  },
-  { 
-    company:"武商奥莱",
-    location:'金银湖科技园12栋',
-    summary:'',
-    detail:[],
-    env:['武商奥莱.jpg'], 
-    closed: true,
-  },
-  { 
-    company:"巷子戏法",
-    location:'金银湖科技园17栋6楼',
-    summary:'',
-    env:['巷子戏法大门.jpg', '巷子戏法电梯.jpg', '巷子戏法盆栽.jpg', '巷子戏法logo.jpg'],    
-    closed: true,
   },
   { 
     company:"冰块厂",
@@ -115,12 +39,6 @@ let jobs=[
     location:'金银湖科技园',
     summary:'招聘甜点制作工，3800元-6000元',
     detail:['樱花糕坊甜点工.jpg'],
-    closed: true,
-  },
-  { 
-    company:"吉米快修",
-    location:'金银湖南三街18-1号',
-    env:['吉米快修.jpg'],    
     closed: true,
   },
 ];
